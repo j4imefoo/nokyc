07a44d55e115f9d55dbb11fe238a26983e8e684a
manuel@beardsystems.com <NbV8-M8I-_9LZcv>
2022-04-03 19:23:34 +0200

    added user agent to avoid kraken 403 error

--- fiat.py
+++ fiat.py
@@ -4,7 +4,7 @@ import json
 
 def getcgprice(fiat, session):
     coingeckoApi = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency='
-    
+
     f = session.get(coingeckoApi + fiat + '&ids=bitcoin')
     priceapi = f.json()
     f.close()
@@ -13,11 +13,12 @@ def getcgprice(fiat, session):
 
 def getkprice(fiat, session):
     krakenApi = 'https://api.kraken.com/0/public/Ticker?pair=XBT'
-    
+
+    session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36'})
     f = session.get(krakenApi + fiat.upper())
     priceapi = f.json()
     f.close()
-    
+
     if fiat in ['eur', 'usd', 'gbp', 'cad']:
         key = 'XXBTZ' + fiat.upper()
     else:
@@ -29,11 +30,10 @@ class Fiat:
     def getfiatprice(fiat, session):
         fiat_kraken = ['eur', 'usd', 'gbp', 'cad', 'aud', 'chf']
         fiat_coingecko = ['brl', 'czk', 'sek', 'nzd', 'dkk', 'pln']
-    
+
         if fiat in fiat_kraken:
             price = getkprice(fiat, session)
         else:
             price = getcgprice(fiat, session)
 
         return price
-
